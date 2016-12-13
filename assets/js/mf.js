b83bc53eeebb5e6076a0eda2bdd915fab21dc415
Patrick Rauls <patrickrauls@Patricks-MacBook-Air.local>
2016-12-13 14:17:01 -0700

    Form Spree Contact Functionality!

--- assets/js/mf.js
+++ assets/js/mf.js
@@ -12,9 +12,30 @@ $('a[href*="#"]:not([href="#"])').click(function() {
     }
 });
 
-//form submission
-$('#contact_form').submit(function(){
-  event.preventDefault();
+//#contact_form submission
+$('#contact_form').submit(function() {
+    event.preventDefault();
+    if ($('#contact_form_insecure').val()) {
+        return false;
+    } else {
+        var message = {
+            sender: $('#contact_form_full_name').val(),
+            email: $('#contact_form_email').val(),
+            phone: $('#contact_form_phone').val(),
+            company: $('#contact_form_company').val(),
+            body: $('#contact_form_message').val()
+        };
+        $.ajax({
+            url: "https://formspree.io/patrauls@gmail.com",
+            method: "POST",
+            data: {
+                message: "From: " + message.sender + " with " + message.company + "\n" + message.phone + "\nReply to: " + message.email + "\n" + message.body
+            },
+            dataType: "json"
+        });
+        $('#contact .container').append('<div id="contact_form_response" class="lead text-center no-display">Thanks! We\'ll be in touch!</div>');
+        $('#contact_form_response').fadeIn(500).delay(2000).fadeOut(500);
+        console.log('I am message', message);
+    }
 });
-
 //navbar add class inverse on 100 from top
